b56be8ad97bac35608954b4e3f84167a79154d60
rinarinas2 <skgcrn2525@gmail.com>
2023-11-08 21:22:25 +0900

    ケーキ追加

---
+++ cakeのコピー/js/script.js
@@ -0,0 +1,15 @@
+$(function(){
+  var pagetop = $('#page-top');
+  pagetop.hide();
+  $(window).scroll(function () {
+     if ($(this).scrollTop() > 300) {
+          pagetop.fadeIn();
+     } else {
+          pagetop.fadeOut();
+     }
+  });
+  pagetop.click(function () {
+     $('body, html').animate({ scrollTop: 0 }, 500);
+     return false;
+  });
+});
